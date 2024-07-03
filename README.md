e13ca25c27bcbb56a9fe3c5b04be788ef234c807
iliags <19849002+iliags@users.noreply.github.com>
2024-07-03 03:58:57 -0700

    Basic bevy and egui setup

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Chip 8
 
 A Chip-8 emulator for practice.
+
+## Building
+
+### PC
+
+```cargo build --release```
+```cargo run --release```
+
+### WASM
+
+- Install trunk using ```cargo install trunk```
+  - This may take a while
+- Build using ```trunk serve --release```
+
+### Build Warnings
+
+If ```wasm-bindgen-cli``` was installed separately, but not update, it may print a tool mismatch warning.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+// TODO: Once the project is more complete, look into https://bevy-cheatbook.github.io/setup/perf.html
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Chip8".to_string(),
+                // Bind to canvas included in `index.html`
+                canvas: Some("#bevy".to_owned()),
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(EguiPlugin)
+        // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
+        // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
+        .add_systems(Update, ui_example_system)
+        .run();
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
 }
