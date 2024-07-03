a5b856b94d86aa189f921a3b537d9f98b4edd236
iliags <19849002+iliags@users.noreply.github.com>
2024-07-03 04:11:09 -0700

    Resolution matching and debug systems

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// disable console on windows for release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
@@ -8,6 +12,8 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Chip8".to_string(),
+                // Set the window size to match the HTML canvas size
+                resolution: (800., 600.).into(),
                 // Bind to canvas included in `index.html`
                 canvas: Some("#bevy".to_owned()),
                 // Tells wasm not to override default event handling, like F5 and Ctrl+R
@@ -20,6 +26,9 @@ fn main() {
         // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
         // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
         .add_systems(Update, ui_example_system)
+
+        // Remove before release
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
