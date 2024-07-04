5528002773a36757779652f8bb3b4e8c526d239b
iliags <19849002+iliags@users.noreply.github.com>
2024-07-04 03:05:46 -0700

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -23,7 +23,11 @@ pub struct App {
     is_running: bool,
 }
 
-enum Registers {
+// Dead code is allowed here because:
+// A) Removing unused registers would mandate manual register mapping
+// B) Unused registers may be used in the future (i.e. for debugging or testing)
+#[allow(dead_code)]
+enum Register {
     V0,
     V1,
     V2,
@@ -117,22 +121,12 @@ impl App {
     // Using i32 for x and y to allow for wrapping around the screen
     fn set_pixel(&mut self, x: i32, y: i32) {
         // If the pixels are out of bounds, wrap them around
-        let mut pos_x = x;
-        let mut pos_y = y;
+        // Wrap around the screen
+        let x = x % SCREEN_WIDTH;
+        let y = y % SCREEN_HEIGHT;
 
-        if x > SCREEN_WIDTH {
-            pos_x -= SCREEN_WIDTH;
-        } else if x < 0 {
-            pos_x += SCREEN_WIDTH;
-        }
-
-        if y > SCREEN_HEIGHT {
-            pos_y -= SCREEN_HEIGHT;
-        } else if y < 0 {
-            pos_y += SCREEN_HEIGHT;
-        }
-
-        let index = (pos_x + pos_y * 64) as usize;
+        // Set the pixel
+        let index = (y * SCREEN_WIDTH + x) as usize;
 
         // Pixels are XORed on the display
         self.display[index] ^= 1;
@@ -198,22 +192,28 @@ impl App {
     }
 
     fn execute_instruction(&mut self, opcode: u16) {
-        // Not all of the opcodes use the upper/lower bits, but enough do to make it worth extracting them
-
-        // Upper bits
+        // Extract the opcode parts
         let x = ((opcode & 0x0F00) >> 8) as usize;
-        // Lower bits
         let y = ((opcode & 0x00F0) >> 4) as usize;
+        let n = (opcode & 0x000F) as u8;
+        let nn = (opcode & 0x00FF) as u8;
+        let nnn = opcode & 0x0FFF;
 
+        // Decode the opcode
         match opcode & 0xF000 {
             0x0000 => {
                 match opcode {
                     0x00E0 => {
+                        // Clear the display
                         self.clear_screen();
                     }
                     0x00EE => {
                         // Return from a subroutine
-                        self.program_counter = self.stack.pop().unwrap();
+                        // TODO: Make this more graceful
+                        self.program_counter = self
+                            .stack
+                            .pop()
+                            .unwrap_or_else(|| panic!("Stack underflow"));
                     }
                     _ => {
                         println!("Unknown 0x0000 opcode: {:#X}", opcode);
@@ -221,26 +221,22 @@ impl App {
                 }
             }
             0x1000 => {
-                // Jump to address NNN
-                self.program_counter = opcode & 0x0FFF;
+                // Jump to address nnn
+                self.program_counter = nnn;
             }
             0x2000 => {
-                // Call subroutine at NNN
+                // Call subroutine at nnn
                 self.stack.push(self.program_counter);
-                self.program_counter = opcode & 0x0FFF;
+                self.program_counter = nnn;
             }
             0x3000 => {
-                // Skip next instruction if Vx == NN
-                let nn = (opcode & 0x00FF) as u8;
-
+                // Skip next instruction if Vx == nn
                 if self.registers[x] == nn {
                     self.program_counter += 2;
                 }
             }
             0x4000 => {
-                // Skip next instruction if Vx != NN
-                let nn = (opcode & 0x00FF) as u8;
-
+                // Skip next instruction if Vx != nn
                 if self.registers[x] != nn {
                     self.program_counter += 2;
                 }
@@ -252,70 +248,61 @@ impl App {
                 }
             }
             0x6000 => {
-                // Set Vx to NN
-                let nn = (opcode & 0x00FF) as u8;
-
+                // Set Vx = nn
                 self.registers[x] = nn;
             }
             0x7000 => {
-                // Add NN to Vx
-                let nn = (opcode & 0x00FF) as u8;
-
+                // Set Vx = Vx + nn
                 self.registers[x] = self.registers[x].wrapping_add(nn);
             }
             0x8000 => {
                 match opcode & 0xF {
                     0x0 => {
-                        // Set Vx to Vy
+                        // Set Vx = Vy
                         self.registers[x] = self.registers[y];
                     }
                     0x1 => {
-                        // Set Vx to Vx | Vy
+                        // Set Vx = Vx OR Vy
                         self.registers[x] |= self.registers[y];
                     }
                     0x2 => {
-                        // Set Vx to Vx & Vy
+                        // Set Vx = Vx AND Vy
                         self.registers[x] &= self.registers[y];
                     }
                     0x3 => {
-                        // Set Vx to Vx ^ Vy
-
+                        // Set Vx = Vx XOR Vy
                         self.registers[x] ^= self.registers[y];
                     }
+                    // TODO: 0x4, 0x5, 0x6, 0x7, and 0xE have quirks associated with them
                     0x4 => {
-                        // Add Vy to Vx, set VF to 1 if there's a carry
-
+                        // Set Vx = Vx + Vy, set VF = carry
                         let (result, overflow) =
                             self.registers[x].overflowing_add(self.registers[y]);
                         self.registers[x] = result;
-                        self.registers[Registers::VF as usize] = overflow as u8;
+                        self.registers[Register::VF as usize] = overflow as u8;
                     }
                     0x5 => {
-                        // Subtract Vy from Vx, set VF to 0 if there's a borrow
-
+                        // Set Vx = Vx - Vy, set VF = NOT borrow
                         let (result, overflow) =
                             self.registers[x].overflowing_sub(self.registers[y]);
                         self.registers[x] = result;
-                        self.registers[Registers::VF as usize] = !overflow as u8;
+                        self.registers[Register::VF as usize] = !overflow as u8;
                     }
                     0x6 => {
-                        // Shift Vx right by 1, set VF to the least significant bit of Vx before the shift
-
-                        self.registers[Registers::VF as usize] = self.registers[x] & 0x1;
+                        // Set Vx = Vx SHR 1
+                        self.registers[Register::VF as usize] = self.registers[x] & 0x1;
                         self.registers[x] >>= 1;
                     }
                     0x7 => {
-                        // Set Vx to Vy - Vx, set VF to 0 if there's a borrow
-
+                        // Set Vx = Vy - Vx, set VF = NOT borrow
                         let (result, overflow) =
                             self.registers[y].overflowing_sub(self.registers[x]);
                         self.registers[x] = result;
-                        self.registers[Registers::VF as usize] = !overflow as u8;
+                        self.registers[Register::VF as usize] = !overflow as u8;
                     }
                     0xE => {
-                        // Shift Vx left by 1, set VF to the most significant bit of Vx before the shift
-
-                        self.registers[Registers::VF as usize] = (self.registers[x] & 0x80) >> 7;
+                        // Set Vx = Vx SHL 1
+                        self.registers[Register::VF as usize] = self.registers[x] >> 7;
                         self.registers[x] <<= 1;
                     }
                     _ => {
@@ -325,44 +312,45 @@ impl App {
             }
             0x9000 => {
                 // Skip next instruction if Vx != Vy
-
                 if self.registers[x] != self.registers[y] {
                     self.program_counter += 2;
                 }
             }
             0xA000 => {
-                // Set index register to NNN
-                self.index_register = opcode & 0x0FFF;
+                // Set I = nnn
+                self.index_register = nnn;
             }
             0xB000 => {
-                // Jump to address NNN + V0
-                self.program_counter =
-                    (opcode & 0x0FFF) + self.registers[Registers::V0 as usize] as u16;
+                // Jump to location nnn + V0
+                self.program_counter = nnn + self.registers[Register::V0 as usize] as u16;
             }
             0xC000 => {
-                // Set Vx to a random number & NN
-                let nn = (opcode & 0x00FF) as u8;
-
-                self.registers[x] = rand::random::<u8>() & nn;
+                // Set Vx = random byte AND nn
+                let mut rng = rand::thread_rng();
+                self.registers[x] = rng.gen::<u8>() & nn;
             }
             0xD000 => {
-                // Draw a sprite at position Vx, Vy with N bytes of sprite data starting at the address stored in I
+                // Draw a sprite at position (Vx, Vy) with N bytes of sprite data starting at the address stored in the index register
+                self.registers[Register::VF as usize] = 0;
+
                 let x = self.registers[x] as i32;
                 let y = self.registers[y] as i32;
-                let height = opcode & 0x000F;
 
-                self.registers[Registers::VF as usize] = 0;
+                let sprite_height = n;
 
-                for yline in 0..height {
-                    let pixel = self.memory[(self.index_register + yline) as usize];
+                for yline in 0..sprite_height {
+                    let pixel = self.memory[(self.index_register + yline as u16) as usize];
 
                     for xline in 0..8 {
                         if (pixel & (0x80 >> xline)) != 0 {
                             if self.display[(x + xline + ((y + yline as i32) * 64)) as usize] == 1 {
-                                self.registers[Registers::VF as usize] = 1;
+                                self.registers[Register::VF as usize] = 1;
                             }
 
-                            self.set_pixel(x + xline, y + yline as i32);
+                            /*
+                            if self.set_pixel(x + xline, y + yline as i32) == 0 {
+                                self.registers[Register::VF as usize] = 1;
+                            }*/
                         }
                     }
                 }
@@ -397,15 +385,10 @@ impl App {
                         self.registers[x] = self.delay_timer;
                     }
                     0x0A => {
-                        // Wait for a key press, store the value of the key in Vx
-                        self.is_running = false;
-
-                        // TODO: Wait for key press
+                        // TODO: Wait for a key press, store the value of the key in Vx
                         println!("Waiting for key press (not implemented yet)");
 
-                        //self.registers[x] = KEY_PRESSED;
-
-                        self.is_running = true;
+                        //self.program_counter -= 2;
                     }
                     0x15 => {
                         // Set the delay timer to Vx
