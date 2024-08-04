65c3cff4bafd6864564db95809acd56505efe8b6
iliags <19849002+iliags@users.noreply.github.com>
2024-08-04 09:51:40 -0700

    Add icon embedding for Windows

---
+++ build.rs
@@ -0,0 +1,15 @@
+//! This file is used to compile the resources for the Windows executable.
+
+fn main() {
+    // Enable the following line to compile the resources only in release mode if it slows down the build.
+    //#[cfg(not(debug_assertions))]
+    {
+        extern crate embed_resource;
+        use std::env;
+        let target = env::var("TARGET").unwrap();
+        if target.contains("windows") {
+            // Set an icon for the executable
+            embed_resource::compile("assets/windows/icon.rc", embed_resource::NONE);
+        }
+    }
+}
