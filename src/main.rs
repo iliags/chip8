7608778664f097721b544ca6b6a36822b9cd75f4
iliags <19849002+iliags@users.noreply.github.com>
2024-08-04 08:43:08 -0700

    App name locale

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
+    use chip8::localization::{LANG, LOCALES};
+    use fluent_templates::Loader;
+
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([800.0, 600.0])
-            .with_min_inner_size([300.0, 220.0])
             .with_icon(
                 // NOE: Adding an icon is optional
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
@@ -19,7 +21,7 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "chip8",
+        &LOCALES.lookup(&LANG, "app_name"),
         native_options,
         Box::new(|cc| Ok(Box::new(chip8::app::app::App::new(cc)))),
     )
