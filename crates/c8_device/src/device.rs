4242fc698eedab660838d021631d1cbde47ceef2
iliags <19849002+iliags@users.noreply.github.com>
2024-08-16 05:50:23 -0700

    Use default trait

--- crates/c8_device/src/device.rs
+++ crates/c8_device/src/device.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 
 /// Chip-8 Device
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct C8 {
     /// The RAM (4kb)
     memory: Memory,
@@ -31,21 +31,6 @@ pub struct C8 {
 
     /// Audio
     pub beeper: Beeper,
-}
-
-impl Default for C8 {
-    fn default() -> Self {
-        Self {
-            memory: Memory::default(),
-            display: Display::default(),
-            cpu: CPU::default(),
-            stack: vec![],
-            is_running: false,
-            keypad: Keypad::default(),
-            quirks: Quirks::default(),
-            beeper: Beeper::new(),
-        }
-    }
 }
 
 impl C8 {
@@ -130,13 +115,8 @@ impl C8 {
         if self.cpu.sound_timer > 0 {
             self.cpu.sound_timer = self.cpu.sound_timer.saturating_sub(1);
 
-            // TODO: Enable when WASM audio is supported
-            #[cfg(debug_assertions)]
-            {
-                self.beeper.play();
-            }
+            self.beeper.play();
         } else {
-            // TODO: Make this more ergonomic (i.e. only pause if it's playing)
             self.beeper.pause();
         }
 
