b7e7a584c27c243eab79e39463a623ef2addf8b6
iliags <19849002+iliags@users.noreply.github.com>
2024-08-22 11:08:28 -0700

    Observer crate

--- crates/observer/src/lib.rs
+++ crates/observer/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! A simple observer pattern implementation.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// Observer module
+pub mod observer;
