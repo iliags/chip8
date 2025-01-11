66cea716ea548d92b2800b036c87978070480828
iliags <19849002+iliags@users.noreply.github.com>
2025-01-10 21:07:59 -0700

    Added roms to test audio implementation

--- src/roms.rs
+++ src/roms.rs
@@ -89,4 +89,12 @@ pub const GAME_ROMS: &[ROM] = &[
         name: "Skyward",
         data: include_bytes!("../assets/games/skyward/skyward.ch8"),
     },
+    ROM {
+        name: "Music Player",
+        data: include_bytes!("../assets/games/xomusicplayer.ch8"),
+    },
+    ROM {
+        name: "Music Player 2",
+        data: include_bytes!("../assets/games/xomusicplayer2.ch8"),
+    },
 ];
