68d11985a52221ed6e125b00c98edb9ab9d5f338
iliags <19849002+iliags@users.noreply.github.com>
2026-04-29 18:43:43 -0700

    Update mod.rs

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -9,15 +9,13 @@ pub mod keyboard;
 
 #[allow(unused_variables)]
 pub fn is_mobile(ctx: &egui::Context) -> bool {
-    #[cfg(target_arch = "wasm32")]
-    {
-        // TODO: Portrait and landscape
-        let screen_size = ctx.content_rect().size();
-        screen_size.x < 550.0
-    }
-
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        false
+    cfg_select! {
+        target_arch = "wasm32" => {
+           let screen_size = ctx.content_rect().size();
+           screen_size.x < 550.0
+        }
+        _ => {
+            false
+        }
     }
 }
